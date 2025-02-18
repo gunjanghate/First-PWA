d53c669e233a452ffd2a2df124fe4c4406f21c88
gunjanghate <ghategunjan@gmail.com>
2025-02-18 17:52:24 +0530

    code added for pwa

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import { Hero } from "@/components";
- 
+import Head from "next/head";
 export default function Home() {
   return (
     <>
-    <Hero />
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#000000" />
+      </Head>
+      <Hero />
     </>
   );
 }
